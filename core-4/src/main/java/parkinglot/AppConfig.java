d00fe032fd58d1b369929852bd1ad36487a31001
Adrian Ibarra Gonzalez <a_drian1@hotmail.es>
2023-10-19 11:11:10 -0600

    Resolved issues to wire Ford with defaultV6 and autowire the carsMap

--- core-4/src/main/java/parkinglot/AppConfig.java
+++ core-4/src/main/java/parkinglot/AppConfig.java
@@ -5,26 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import parkinglot.cars.*;
 import parkinglot.engines.Engine;
 import parkinglot.engines.FourCylinderEngine;
 import parkinglot.engines.V12Engine;
 import parkinglot.engines.V6Engine;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import static java.util.Map.entry;
-import static org.springframework.aop.interceptor.ExposeBeanNameAdvisors.getBeanName;
 
 @Configuration
 public class AppConfig {
-    @Autowired
-    Map<String,Car> carMap;
     @Bean
-    public FreeParkingLot freeParkingLot() {
+    public FreeParkingLot freeParkingLot(@Autowired Map<String,Car> carMap) {
         return new FreeParkingLot(carMap);
     }
     @Bean
@@ -49,17 +42,18 @@ public class AppConfig {
 
     //New Beans
     @Bean
+    @Primary
     public Engine defaultV6Engine(){
         return new V6Engine();
     }
 
     @Bean("4X4")
-    public Car _4X4(){
-        return new FordBronco(defaultV6Engine());
+    public Car _4X4(Engine engine){
+        return new FordBronco(engine);
     }
 
     @Bean
-    public Car jurassicCar(){
-        return new FordRaptor(defaultV6Engine());
+    public Car jurassicCar(Engine engine){
+        return new FordRaptor(engine);
     }
 }
